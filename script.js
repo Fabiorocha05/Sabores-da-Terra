d5b4a2f550f657ac2b4c6e56f3de470901af9518
Fabiorocha05 <fabinhorochinha1997@gmail.com>
2025-03-27 09:49:22 +0000

    Loja online

---
+++ script.js
@@ -0,0 +1,11 @@
+let show = true;
+const menucontent = document.querySelector(".content");
+const menutoggle = menucontent.querySelector(".menu-toggle");
+
+
+menutoggle.addEventListener("click", () => {
+
+    menucontent.classList.toggle("on", show);
+    show = !show; 
+
+});
